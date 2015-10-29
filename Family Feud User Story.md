4196bebb8ca27e834a47871ef65d1fb038f3d24c
Lilly Ghassemieh <lillygh@gmail.com>
2015-10-28 23:16:36 -0700

    initial commit; showed first question

---
+++ Family Feud User Story.md
@@ -0,0 +1,49 @@
+Family Feud:
+
+
+On load:
+1. play intro music
+
+Game:
+
+1. Users required to enter their names to begin.
+2. 
+
+1. Toss-up round
+	- 2 people playing
+	- display 1 question 
+	- display number of answers on the board
+	- 2 active keys for the buzzer
+	- timer after buzzer for the answer
+	- highest rated answer wins
+	- if both lose, moves to the next 2 players from each family
+	- input field to type answer
+	- winner of toss-up chooses to pass or play
+	- can't change answers once inputted
+
+2. Game Round 1
+	- 3 family members (array)
+	- Each family member answers question, rotate through array
+	- timer per person
+	- display same question
+	- input field for answers
+	- display answers on board or strike
+	- 3 strikes for family 1 before family 2 can steal all points with any correct answer, otherwise, family 1 wins round
+	- reveal all remaining answers at the end of each round
+	- can't change answers once inputted
+
+3. Toss-up round (repeat step 1)
+
+4. Game Round 2 (repeat step 2)
+	- points are doubled
+	- family with highest points goes to fast money round
+
+5. Fast Money Round
+	- 2 people selected from winning family
+	- 3 questions
+	- input field for user answers (display answers on board)
+	- timer for each person
+	- can't change answers once inputted
+	- win game if over x amount of points
+
+
---
+++ main.js
@@ -0,0 +1,173 @@
+/*
+	ENGINE FOR THE GAME
+	0. Players enter their names
+	1. Display question, display counter-down timer
+	2. Hide answers in each answer slot
+	3. Player1 can enter answer
+	4. If answer is correct, flip the slot containing the answer, award points for that answer to player1
+	6. If player1 get all answers, player1 get all points from this round, round ends, player1 starts next round
+	7. If player1 makes a wrong guess, player1 get "strike" (X).
+	8. Player1 is allowed 3 strikes, after 3rd strike, player2 starts, player 2 has 1 guess.
+	   8.1 If player2 guesses any remaining answer,
+	   		8.1.1 player2 wins round
+	   		8.1.2 player2 get all points of player1 from that round,
+	   8.2 If player doesn't guess any remaining answer
+	   		8.2.1 Player1 wins round
+	   		8.2.1 Player1 gets all the points from his correct guesses
+	9. Winner of current round starts next round
+	10. Each game has 3 rounds, after round 3, player with highest score wins
+*/
+
+//github, user stories, switch question
+
+var qData = [{question: 'Name a word that most people yell at their dogs',
+			  answers: ['No', 'Sit', 'Stop', 'Down', 'Fetch', 'Jump'],
+			  scores: [27,23,14,7,6,5,4]
+			 },
+
+			{question: 'Name a fruit you can eat with one bite.',
+			  answers: ['Strawberry', 'Grape', 'Blueberry', 'Raspberry', 'Plum', 'Banana'],
+			  scores: [56,17,9,3,2,1]
+			},
+
+			{question: 'Name a holiday when people have parties.',
+			  answers: ['Christmas', 'Thanksgiving', 'Halloween', "4th of July", 'Labor Day', 'New Years Eve'],
+			  scores: [35,21,11, 3,2,1]
+			}
+];
+
+$("#startgamebutton").click(function() {
+	var question1 = qData[0].question
+	$("#instruction1").hide();
+	$("#instruction2").show().delay(3000).fadeOut();
+	$("#question").show().text(question1);
+	$(".startGame").hide();
+	$("#answerArea").show()
+});
+
+
+//Game Timer:
+	// var game = {score: 0, ellapsedTime: 4, messages: ''};
+	// intervalID = setInterval(function(){
+	// 	game.ellapsedTime--;
+	// 	$('#game-time').text(' ' + game.ellapsedTime);
+	// 	if (game.ellapsedTime<=0) {
+	// /*		alert('TIME UP BITCH!');
+	// */        clearInterval(intervalID);
+	//     };
+	// },1000);
+
+
+//Family 1 Last Name switch from input field to name
+
+
+	$("#family1-button").click(function() {
+	  var playerLastName = $("#family1-name").val();
+	  $("#family1-name").hide();
+	  $("#family1-button").hide();
+
+	  $("#family1-name-output").show().text(playerLastName + " Family");
+	});
+
+
+
+//Family 2 Last Name switch from input field to name
+
+
+
+	$("#family2-button").click(function() {
+	  var playerLastName = $("#family2-name").val();
+	  $("#family2-name").hide();
+	  $("#family2-button").hide();
+
+	  $("#family2-name-output").show().text(playerLastName + " Family");
+	});
+
+
+
+//Player 1 Last Name switch from input field to name
+
+
+	$("#player1-button").click(function() {
+	  var playerName = $("#player1-input").val();
+	  $("#player1-input").hide();
+	  $("#player1-button").hide();
+
+	  $("#player1-name-output").show().text(playerName);
+	});
+
+
+//Player 2 Last Name switch from input field to name
+
+
+	$("#player2-button").click(function() {
+	  var playerName = $("#player2-input").val();
+	  $("#player2-input").hide();
+	  $("#player2-button").hide();
+
+	  $("#player2-name-output").show().text(playerName);
+	});
+
+
+//Player 3 Last Name switch from input field to name
+
+
+	$("#player3-button").click(function() {
+	  var playerName = $("#player3-input").val();
+	  $("#player3-input").hide();
+	  $("#player3-button").hide();
+
+	  $("#player3-name-output").show().text(playerName);
+	});
+
+
+//Player 4 Last Name switch from input field to name
+
+
+	$("#player4-button").click(function() {
+	  var playerName = $("#player4-input").val();
+	  $("#player4-input").hide();
+	  $("#player4-button").hide();
+	  $("#player4-name-output").show().text(playerName);
+	});
+
+	function checkAnswer(){
+		if(!$('.input-buttons').is(':visible')) {
+			$('#startGameButton').click(function(){
+				$('messages').hide();
+				$("#question1").show();
+		})
+		}
+	};
+
+//answer input values
+	//
+	// function checkAnswer(){
+	// 	var $q1a1 = $('#frontAnswer1')
+	//  var $q1a2 = $('#frontAnswer2')
+	//  var $q1a3 = $('#frontAnswer3')
+	//  var $q1a4 = $('#frontAnswer4')
+	//  var $q1a5 = $('#frontAnswer5')
+	//  var playerAnswer = $("#answerInput").val().toLowerCase();
+	//
+	// 	if ((playerAnswer === "no") || (playerAnswer === "no dog")) {
+	//     	$q1a1.html('<div class="answer-text">No</div>');
+	//     } else if
+	//     	((playerAnswer === "sit") || (playerAnswer === "sit down")) {
+	//     	$q1a2.html('<div class="answer-text">Sit</div>');
+	//     } else if
+	//     	(playerAnswer === "stop") {
+	//     	$q1a3.html('<div class="answer-text">Stop</div>');
+	//     } else if
+	//     	(playerAnswer === "down") {
+	//     	$q1a4.html('<div class="answer-text">Down</div>');
+	// 	} else if
+	//     	(playerAnswer === "fetch") {
+	//     	$q1a5.html('<div class="answer-text">Fetch</div>');
+	//     } else if
+	//     	(playerAnswer === "off") {}
+	//     	$q1a6.html('<div class="answer-text">Off</div>');
+	//     } else if
+	//     	$("#1strike").show().hide(3000);
+	//     };
+	//
